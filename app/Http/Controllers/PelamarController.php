eff904e0b273bbefed3df344570ac8d5710f3b97
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2019-03-28 09:32:00 +0800

    send docusign

--- app/Http/Controllers/PelamarController.php
+++ app/Http/Controllers/PelamarController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Mail\PelamarProses;
 use App\Pelamar;
 use Validator;
+use DocuSign;
 use Mail;
 
 class PelamarController extends Controller
@@ -29,11 +30,37 @@ class PelamarController extends Controller
     $state = decrypt($state);
     $pelamar = Pelamar::findOrFail($id);
     $pelamar->update(['status' => $state]);
-    $this->sendMailPelamar($pelamar, $state);
+    if ($state == 1) $this->sendMailPelamar($pelamar, $state);
+    if ($state == 3) $this->prosesSign($pelamar);
     return redirect()->back();
   }
 
   private function sendMailPelamar($pelamar, $status){
     return Mail::to($pelamar->email)->send(new PelamarProses($pelamar, $status));
+  }
+
+  private function prosesSign($pelamar){
+    $client = DocuSign::create();
+    $templateRole = $client->templateRole([
+    	'email' 	=> $pelamar->email,
+    	'name'  	=> $pelamar->nama,
+    	'role_name' => 'Staff'
+    ]);
+    $envelopeDefinition = $client->envelopeDefinition([
+    	'status'         => 'sent',
+    	'email_subject'  => "Kontrak Kerja",
+    	'template_id'    => "4d42fd24-8938-4248-8342-ff7811a1bd58",
+    	'template_roles' => [
+    		$templateRole
+    	],
+    ]);
+    $envelopeOptions = $client->envelopes->createEnvelopeOptions([
+			'merge_roles_on_draft' => "false"
+  	]);
+    try {
+      $client->envelopes->createEnvelope($envelopeDefinition, $envelopeOptions);
+    } catch (DocuSign\eSign\ApiException $e){
+      dd("Error connecting Docusign : " . $e->getResponseBody()->errorCode . " " . $e->getResponseBody()->message);
+    }
   }
 }
